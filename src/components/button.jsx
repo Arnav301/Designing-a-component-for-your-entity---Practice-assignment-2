fb93b26e78d8a87865d06aa72c9135356c1cd306
Arnav Verma <arnavverma3016@gmail.com>
2025-03-12 10:27:12 +0530

    Updated

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,21 @@
 // write button card here
+
+import { useState } from "react";
+
+function Button(){
+    const [colour, setColor]=useState('crimson')
+    return(
+        <div>
+            <button style ={{
+                backgroundColor: colour
+            }} onClick={()=>{
+                setColor((e)=>e === 'crimson' ? 'blue' : 'crimson')
+            }}
+            >Buy Now
+            </button>
+        </div>
+
+    ); 
+}
+
+export default Button;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,27 @@
 // write product card here
+
+import Button from "./button";
+
+function productCard(){
+const productImage = 'https://dlcdnwebimgs.asus.com/files/media/8CF497C8-1A77-404F-ABF1-85822D3BE077/v1/img/gaming/pd.jpg'
+const productName = 'Asus Strix G18'
+const Price = '189000'
+    return(
+        <div>
+            <p>{productName}</p>
+           <img style={{ width: "300px" }}
+           src={productImage} />
+            <p>{Price}</p>
+            <Button/>
+        </div>
+    );
+}
+
+export default productCard;
+
+
+
+
+// product image
+// - product name 
+// - price
